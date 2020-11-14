dcb45da7ca991b3f7dd2cabb30f8fbb44aaaf210
家子智成 <>
2020-11-14 18:58:06 +0900

    皿を持つIK実装

---
+++ Assets/0_MyAssets/Scripts/Game/HoldDishIK.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// https://gametukurikata.com/animationanimator/bringik
+/// </summary>
+public class HoldDishIK : MonoBehaviour
+{
+    [SerializeField] Transform leftHandTransform;
+    [SerializeField] Transform rightHandTransform;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnAnimatorIK()
+    {
+        //　左手、右手のIK設定
+        animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+        animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+        animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+        animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandTransform.position);
+        animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandTransform.rotation);
+        animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandTransform.position);
+        animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandTransform.rotation);
+    }
+}
